6aecd227ee7ad7b0caa290f2b2f97b3191be8015
Erick Suárez <42328586+esvarez@users.noreply.github.com>
2026-09-17 19:36:27 -0600

    83 add shadcdn (#84)
    * Add tailwindcss
    
    * Manual installation
    
    * Install shadcdn
    
    ---------
    
    Co-authored-by: Erick Suarez <ericks@Ericks-MacBook-Pro.local>

--- web/src/layout/Layout.tsx
+++ web/src/layout/Layout.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
 
 function Layout() {
   return (
     <div className="app-shell">
       <header className="app-header">
         <nav>Nudge</nav>
+        <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <Button>Click me</Button>
       </header>
       <main>
         <Outlet />
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,19 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': dirname,
+    },
+  },
   server: {
     proxy: {
       '/api': {
